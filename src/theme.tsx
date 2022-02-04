9a46b96581c596ab3ea02840250c37c941aedae9
seungdang123 <seungdang123@gmail.com>
2022-02-05 02:22:06 +0900

    first commit

---
+++ src/theme.tsx
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#fff",
+  accentColor: "#8c7ae6",
+  boxColor: "rgba(0, 0, 0, 0.5);",
+};
+
+export const theme = {
+  darkTheme,
+};
